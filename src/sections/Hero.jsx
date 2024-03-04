28386d6c2f7b667d967aa0b8ae6fe7be6136fbd0
MuhammadFahrulazmi322 <fazmi322@gmail.com>
2024-03-04 16:37:25 +0700

    finish hero section, popularProducts, & SuperQuality

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
             <div className="relative
             xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
                 <p className="text-xl font-montserrat text-coral-red">Our Summer Collections</p>
-                <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
+                <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
                     <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span>
                     <br />
                     <span className="text-coral-red inline-block mt-3">Nike</span>
@@ -28,7 +28,7 @@ const Hero = () => {
                 <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
                     {statistics.map((stat) => (
                         <div key={stat.label}>
-                            <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
+                            <p className="md:text-4xl text-3xl font-palanquin font-bold">{stat.value}</p>
                             <p className="leading-7 font-montserrat text-slate-gray">{stat.label}</p>
                         </div>
                     ))}
@@ -46,11 +46,11 @@ const Hero = () => {
                 />
                 <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%]
                  sm:left-[10%] max-sm:px-6">
-                    {shoes.map((shoe) => (
-                        <div key={shoe}>
+                    {shoes.map((images,index) => (
+                        <div key={index}>
                             <ShoeCard
-                                imgURL={shoe}
-                                changeBigShoeImage={(shoe) => { setbigShoeImg(shoe) }}
+                                imgURL={images}
+                                changeBigShoeImage={(images) => { setbigShoeImg(images) }}
                                 bigShoeImg={bigShoeImg}
                             />
                         </div>
--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,6 +1,39 @@
+import { shieldTick, truckFast } from "../assets/icons"
+
 const Services = () => {
     return (
-        <div>Service</div>
+        <section className="max-container">
+            <div className="flex flex-row gap-10 max-lg:flex-col justify-center items-center">
+                <div className="flex flex-1 flex-col gap-2">
+                    <div>
+                        <img src={truckFast} className="bg-coral-red p-2 rounded-full" alt=""/>
+                    </div>
+                    <h3 className="mt-4 font-bold font-palanquin text-2xl">Free Shipping</h3>
+                    <p className="mt-4 info-text">
+                        Enjoy seamless Shopping with our complimentary shipping
+                    </p>
+                </div>
+                <div className="flex flex-1 flex-col">
+                    <div>
+                        <img src={shieldTick} className="bg-coral-red p-2 rounded-full" alt=""/>
+                    </div>
+                    <h3 className="mt-4 font-bold font-palanquin text-2xl">Free Shipping</h3>
+                    <p className="mt-4 info-text">
+                        Enjoy seamless Shopping with our complimentary shipping
+                    </p>
+                </div>
+                <div className="flex flex-1 flex-col">
+                    <div>
+                        <img src={truckFast} className="bg-coral-red p-2 rounded-full" alt=""/>
+                    </div>
+                    <h3 className="mt-4 font-bold font-palanquin text-2xl">Free Shipping</h3>
+                    <p className="mt-4 info-text">
+                        Enjoy seamless Shopping with our complimentary shipping
+                    </p>
+                </div>
+                
+            </div>
+        </section>
     )
 }
 
