823c5e5ddf6658761ad3d90321ba041296711dfa
MuhammadFahrulAzmi322 <fazmi322@gmail.com>
2024-03-04 23:41:44 +0700

    finish landing-page

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,37 +1,19 @@
 import { shieldTick, truckFast } from "../assets/icons"
+import ServiceCard from "../components/ServiceCard"
+import { services } from "../constants"
+
 
 const Services = () => {
     return (
-        <section className="max-container">
+        <section className="max-container flex justify-center flex-wrap gap-9">
             <div className="flex flex-row gap-10 max-lg:flex-col justify-center items-center">
-                <div className="flex flex-1 flex-col gap-2">
-                    <div>
-                        <img src={truckFast} className="bg-coral-red p-2 rounded-full" alt=""/>
-                    </div>
-                    <h3 className="mt-4 font-bold font-palanquin text-2xl">Free Shipping</h3>
-                    <p className="mt-4 info-text">
-                        Enjoy seamless Shopping with our complimentary shipping
-                    </p>
-                </div>
-                <div className="flex flex-1 flex-col">
-                    <div>
-                        <img src={shieldTick} className="bg-coral-red p-2 rounded-full" alt=""/>
-                    </div>
-                    <h3 className="mt-4 font-bold font-palanquin text-2xl">Free Shipping</h3>
-                    <p className="mt-4 info-text">
-                        Enjoy seamless Shopping with our complimentary shipping
-                    </p>
-                </div>
-                <div className="flex flex-1 flex-col">
-                    <div>
-                        <img src={truckFast} className="bg-coral-red p-2 rounded-full" alt=""/>
-                    </div>
-                    <h3 className="mt-4 font-bold font-palanquin text-2xl">Free Shipping</h3>
-                    <p className="mt-4 info-text">
-                        Enjoy seamless Shopping with our complimentary shipping
-                    </p>
-                </div>
-                
+                {services.map((service) => (
+                    <ServiceCard
+                        key={service.label}
+                        {...service}
+                    />
+                ))}
+
             </div>
         </section>
     )
